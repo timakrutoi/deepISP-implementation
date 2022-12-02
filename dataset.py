4725f07803e9848e10c30bae1f7da2d121b4e37f
Timofei <timaoleinik@gmail.com>
2022-12-02 17:19:33 +0300

    Updated dataset.py

--- dataset.py
+++ dataset.py
@@ -1,41 +1,51 @@
-from torch.utils import Dataset
-from torchvision.io import read_image
+from os import listdir, sep
+
 from torchvision import transforms
+from torchvision.io import read_image
+from torch.utils import Dataset
 from torch.utils.data import DataLoader
 
 
 class S7Dataset(Dataset):
-    def __init__(self, annotations_file, img_dir, transform=None, target_transform=None):
-        self.img_labels = pd.read_csv(annotations_file)
-        self.img_dir = img_dir
+    def __init__(self, directory, mode='m', transform=None, target_transform=None):
+        self.directory = directory
+
         self.transform = transform
         self.target_transform = target_transform
 
+        self.dng = '.dng'
+        self.jpg = '.jpg'
+
+        if mode == 'm':
+            self.mode = 'medium_exposure'
+        elif mode == 's':
+            self.mode = 'short_exposure'
+            self.jpg = '1.jpg'
+
+
     def __len__(self):
-        return len(self.img_labels)
+        return len(os.listdir(self.directory))
 
     def __getitem__(self, idx):
-        img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
+        l = os.listdir(self.directory)
+        i_img = read_image(sep.join([self.directory, l[idx]], f'{self.mode}{self.dng}'))
+        o_img = read_image(sep.join([self.directory, l[idx]], f'{self.mode}{self.jpg}'))
 
-        image = transforms.Resize((300, 300))(read_image(img_path))
-
-        label = self.img_labels.iloc[idx, 1]
         if self.transform:
             image = self.transform(image)
         if self.target_transform:
             label = self.target_transform(label)
-        return image.float(), label
+
+        return i_img.float(), o_img.float()
 
 
-def get_data(data_path, batch_size=64):
+def get_data(data_path='../dataset/S7-ISP-Dataset', batch_size=64):
     train_data = S7Dataset(
-        annotations_file=data_path + 'annotations_train.csv',
-        img_dir=data_path + 'images'
+        img_dir=data_path
     )
 
     test_data = S7Dataset(
-        annotations_file=data_path + 'annotations_test.csv',
-        img_dir=data_path + 'images'
+        img_dir=data_path
     )
 
     train_loader = DataLoader(
