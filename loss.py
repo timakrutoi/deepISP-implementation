37157ddaa787cbc7820f57b6763fe83f77762cc2
Timofei <timaoleinik@gmail.com>
2023-01-25 19:04:10 +0300

    Turned off SSIM

--- loss.py
+++ loss.py
@@ -9,14 +9,14 @@ class deepISPloss():
         self.alpha = alpha
         self.MSSSIM = MSSSIM()
         self.l1 = torch.nn.L1Loss()
-    
+
     def __call__(self, x, target):
         lab_x = rgb_to_lab(x).float()
         lab_tar = rgb_to_lab(target).float()
 
-        # res = torch.randn(1, requires_grad=True)
         res = (1 - self.alpha) * self.l1(lab_x, lab_tar)
         # take only first channel to MS-SSIM
+        # turned ssim off coz it doesnt work
         # res =     self.alpha  * (self.MSSSIM(lab_x[:, :1, :, :], lab_tar[:, :1, :, :]))
 
         return res
