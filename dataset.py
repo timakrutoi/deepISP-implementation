23b6e51441f9726ad08ecf8cf0c1b20fee92b6dc
Timofei <timaoleinik@gmail.com>
2023-01-26 20:24:26 +0300

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -2,8 +2,6 @@ from os import listdir, sep
 import numpy as np
 import torch
 
-from torchvision import transforms
-# from torchvision.io import read_image
 from torch.utils.data import DataLoader
 from torch.utils.data import Dataset
 
@@ -15,22 +13,24 @@ from demosaic import demosaicing_Bayer_bilinear
 
 
 class S7Dataset(Dataset):
-    def __init__(self, directory, mode, target, factor, crop_size, norm):
+    def __init__(self, directory, mode, target,
+                 factor, crop_size, norm, rand=False):
         self.directory = directory
 
         self.raw_transform = demosaicing_Bayer_bilinear
         self.crop_size = crop_size
         self.norm = norm
+        self.rand = rand
 
         self.dng = '.dng'
         self.jpg = '.jpg'
 
-        self.l = len(listdir(self.directory))
+        tmp_len = len(listdir(self.directory))
 
         if mode == 'train':
-            self.len = 0, int(self.l * factor)
+            self.len = 0, int(tmp_len * factor)
         if mode == 'test':
-            self.len = int(self.l * factor), self.l
+            self.len = int(tmp_len * factor), tmp_len
 
         if target == 'm':
             self.target = 'medium_exposure'
@@ -42,40 +42,52 @@ class S7Dataset(Dataset):
         return self.len[1] - self.len[0]
 
     def __getitem__(self, idx):
-        l = listdir(self.directory)
+        dirs = listdir(self.directory)
 
-        i_img = io.imread(sep.join([self.directory, l[idx + self.len[0]], f'{self.target}{self.dng}'])).astype('float32')
-        o_img = io.imread(sep.join([self.directory, l[idx + self.len[0]], f'{self.target}{self.jpg}'])).astype('float32')
+        i1p = sep.join([self.directory,
+                        dirs[idx + self.len[0]],
+                        f'{self.target}{self.dng}'])
+        i2p = sep.join([self.directory,
+                        dirs[idx + self.len[0]],
+                        f'{self.target}{self.jpg}'])
+
+        i_img = torch.tensor(io.imread(i1p).astype('float'))
+        o_img = torch.tensor(io.imread(i2p).astype('float'))
 
         if self.norm:
-            i_img = self.raw_transform((i_img - 512) / 512)
+            i_img = (i_img - 512) / 512
             o_img = (o_img - 128) / 128
-        else:
-            i_img = self.raw_transform(i_img)
-            # o_img = o_img
-        
+
+        i_img = self.raw_transform(i_img)
+
         old_shape = i_img.shape
-        new_shape = old_shape[2], self.crop_size, self.crop_size
-        
-        x = np.random.randint(0, old_shape[0] - self.crop_size)
-        y = np.random.randint(0, old_shape[1] - self.crop_size)
-        
+
+        if self.rand:
+            x = np.random.randint(0, old_shape[0] - self.crop_size)
+            y = np.random.randint(0, old_shape[1] - self.crop_size)
+        else:
+            # debugging thing
+            x, y = 1779, 982
+
         slice_x = slice(x, x + self.crop_size)
         slice_y = slice(y, y + self.crop_size)
-        
-        i_img = torch.tensor(i_img[slice_x, slice_y, :].copy())
-        if (old_shape[0], old_shape[1]) != (o_img.shape[0], o_img.shape[1]):            
-#             print('Bad shape detected', old_shape, o_img.shape)
+
+        i_img = i_img[slice_x, slice_y, :].clone().detach()
+        if (old_shape[0], old_shape[1]) != (o_img.shape[0], o_img.shape[1]):
+            # print('Bad shape detected', old_shape, o_img.shape)
             slice_x, slice_y = slice_y, slice_x
-        o_img = torch.tensor(o_img[slice_x, slice_y, :].copy())
-                
-        i_img = i_img.reshape(new_shape)
-        o_img = o_img.reshape(new_shape)
+        o_img = o_img[slice_x, slice_y, :].clone().detach()
+
+        i_img = i_img.permute(2, 0, 1)
+        o_img = o_img.permute(2, 0, 1)
 
         return i_img.float(), o_img.float()
 
 
-def get_data(data_path, batch_size, target='m', factor=0.7, crop_size=256, norm=False):
+def get_data(data_path, batch_size,
+             target='m', factor=0.7,
+             crop_size=256, norm=False):
+
     train_data = S7Dataset(
         directory=data_path,
         mode='train',
@@ -97,7 +109,7 @@ def get_data(data_path, batch_size, target='m', factor=0.7, crop_size=256, norm=
     train_loader = DataLoader(
         train_data,
         batch_size=batch_size,
-        shuffle=True
+        shuffle=False
     )
     test_loader = DataLoader(
         test_data,
