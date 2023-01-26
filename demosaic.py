7c1c7a3796d2ab6f7d768f76465eda60d5a43a32
Timofei <timaoleinik@gmail.com>
2023-01-26 20:24:40 +0300

    Update demosaic.py

--- demosaic.py
+++ demosaic.py
@@ -1,9 +1,9 @@
-from scipy.ndimage import convolve
 import numpy as np
+from scipy.ndimage import convolve
+import torch
 
 
 def masks_bayer(shape, pattern):
-    
     channels = {channel: np.zeros(shape, dtype="bool") for channel in "RGB"}
     for channel, (y, x) in zip(pattern, [(0, 0), (0, 1), (1, 0), (1, 1)]):
         channels[channel][y::2, x::2] = 1
@@ -11,46 +11,42 @@ def masks_bayer(shape, pattern):
     return tuple(channels.values())
 
 
-def demosaicing_Bayer_bilinear(x, pattern="RGGB"):
-
-    CFA = x.astype('float')
+def demosaicing_Bayer_bilinear(CFA, pattern="RGGB"):
     R_m, G_m, B_m = masks_bayer(CFA.shape, pattern)
 
-    H_G = np.array([
+    H_G = torch.tensor([
             [0, 1, 0],
             [1, 4, 1],
             [0, 1, 0],
-        ], dtype='float')
+        ], dtype=torch.float32)
 
-    H_RB = np.array([
+    H_RB = torch.tensor([
             [1, 2, 1],
             [2, 4, 2],
             [1, 2, 1],
-        ], dtype='float')
+        ], dtype=torch.float32)
 
     R = convolve(CFA * R_m, H_RB / 4, mode='mirror')
-    G = convolve(CFA * G_m, H_G  / 4, mode='mirror')
+    G = convolve(CFA * G_m, H_G / 4, mode='mirror')
     B = convolve(CFA * B_m, H_RB / 4, mode='mirror')
+
+    R = torch.tensor(R)
+    G = torch.tensor(G)
+    B = torch.tensor(B)
 
     del R_m, G_m, B_m, H_RB, H_G
 
-    return np.stack([R, G, B], 2)
+    return torch.stack([R, G, B], 2)
 
 
 if __name__ == '__main__':
-    img_shape = (3,3)
-    # img = np.ones(img_shape)
-    pattern = 'RGGB'
-    
-    masks = masks_bayer(img_shape, pattern)
-
-    for i in masks:
-        print(i)
+    import matplotlib.pyplot as plt
 
     s = 10
-    f = 1023.
     img = np.ones((s, s))
-    deb = my_bil(img).reshape((s, s, 3))
+    pattern = 'RGGB'
+
+    deb = demosaicing_Bayer_bilinear(img, pattern)
 
     plt.imshow(deb)
     plt.show()
