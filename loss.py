b6f2e3e20c7f5037dfed4f9d6104c4a7b529cf6b
Timofei <timaoleinik@gmail.com>
2023-01-29 03:34:24 +0300

    Update loss.py

--- loss.py
+++ loss.py
@@ -17,13 +17,14 @@ class deepISPloss():
         self.norm = norm
 
     def __call__(self, x, target):
-        lab_x = rgb_to_lab(self.norm(x)).float()
-        lab_tar = rgb_to_lab(self.norm(target)).float()
+        lab_x = rgb_to_lab(self.norm(x))
+        lab_tar = rgb_to_lab(self.norm(target))
+        return self.l1(lab_x, lab_tar)
 
-        res = (1 - self.alpha) * self.l1(lab_x, lab_tar)
+        # res = (1 - self.alpha) * self.l1(lab_x, lab_tar)
         # take only first channel to MS-SSIM
         # turned ssim off coz it doesnt work
         # res +=     self.alpha  * (self.MSSSIM(lab_x[:, :1, :, :],
         #                                       lab_tar[:, :1, :, :]))
 
-        return res
+        # return res
