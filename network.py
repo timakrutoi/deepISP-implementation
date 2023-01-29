73e1e4f39efe2384323bf15217910c7ab32c062a
Timofei <timaoleinik@gmail.com>
2023-01-29 03:37:51 +0300

    Fixed comments in network.py

--- network.py
+++ network.py
@@ -4,29 +4,28 @@ import torch.nn as nn
 
 def Tform(I, W):
     b, c, h, w = I.shape
+    # reshape W from (b, 30) to (b, 3, 10)
     # somehow (b, 3, 10) doesnt work, but (b, 10, 3) does
     # W = W.reshape(b, 3, 10)
-    W = W.reshape((b, 10, 3)).permute((0, 2, 1))
+    W = W.reshape((b, 10, 3)).transpose(1, 2)
 
     # adding 4th channel with all 1
-    I = torch.cat((I, torch.ones((b, 1, h, w))), dim=1)
-    # I = I.reshape((b, 1, 4, h, w))
-    # g = I.transpose(1, 2)
+    I = torch.cat((I, torch.ones((b, 1, h, w), dtype=torch.float)), dim=1)
 
-    # matmul (b, 4, 1, h, w)*(b, 1, 4, h, w) = (b, 4, 4, h, w)
-    # n = torch.einsum('bceij,befij->bcfij', g, I)
-    n = torch.einsum('beij,bfij->befij', I, I)
+    # matmul (b, 4, h, w)*(b, 4, h, w) = (b, 4, 4, h, w)
+    n = torch.einsum('beij,bfij->bfeij', I, I)
 
     # get vectorized triu
     n = torch.flatten(n, 1, 2)[:, [0, 1, 2, 3, 5, 6, 7, 10, 11, 15], :, :]
-    n = n.reshape((b, 10, 1, h, w))
 
-    # matmul (b, 3, 10)*(b, 10, 1, h, w) = (b, 3, h, w)
-    n = torch.einsum('bwc,bcfij->bwij', W, n)
+    # matmul (b, 3, 10)*(b, 10, h, w) = (b, 3, h, w)
+    n = torch.einsum('bwc,bcij->bwij', W, n)
 
     # wtf?
-    n[n > 1] = 1
-    n[n < -1] = -1
+    if torch.any(n > 1) or torch.any(n < -1):
+        # print(f'Bad values {n.min()} - {n.max()}')
+        n[n > 1] = 1
+        n[n < -1] = -1
 
     return n
 
@@ -105,7 +104,7 @@ class DeepISP(nn.Module):
                                             stride=2, padding=0))
 
         # append global pooling on high level to get 1x1x64 shape
-        # current shape = (N/4^n_hl)*(M/4^n_hl)*64
+        # current shape is (N/4^n_hl, M/4^n_hl, 64)
         self.highlevel.append(GlobalPool2d())
 
         self.highlevel.append(nn.Linear(64, 30))
