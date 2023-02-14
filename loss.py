14e39d1475b499511c9a774ab793f77ff952e84b
Timofei <timaoleinik@gmail.com>
2023-02-14 23:33:33 +0300

    Update loss.py, turned on MSSSIM

--- loss.py
+++ loss.py
@@ -4,11 +4,12 @@ from torchmetrics import MultiScaleStructuralSimilarityIndexMeasure as MSSSIM
 
 from utils import Norm
 
+
 class deepISPloss():
-    def __init__(self, alpha=0.5, norm_mode='simple'):
+    def __init__(self, device='cpu', alpha=0.5, norm_mode='simple'):
         self.alpha = alpha
         self.l1 = torch.nn.L1Loss()
-        self.MSSSIM = MSSSIM()
+        self.MSSSIM = MSSSIM().to(torch.device(device))
 
         self.norm_mode = norm_mode
         self.norm = Norm(mode='positive')
@@ -16,15 +17,15 @@ class deepISPloss():
     def __call__(self, x, target):
         # get (max, min) vals that were used in dataset norm
         b = Norm.get_bounds_by_mode(self.norm_mode)
+        x = self.norm(x, bounds_before=b)
+        t = self.norm(target, bounds_before=b)
 
-        lab_x = rgb_to_lab(self.norm(x, bounds_before=b))
-        lab_tar = rgb_to_lab(self.norm(target, bounds_before=b))
-        return self.l1(lab_x, lab_tar)
+        lab_x = rgb_to_lab(x)
+        lab_tar = rgb_to_lab(t)
 
-        # res = (1 - self.alpha) * self.l1(lab_x, lab_tar)
+        res = (1 - self.alpha) * self.l1(lab_x, lab_tar)
         # take only first channel to MS-SSIM
-        # turned ssim off coz it doesnt work
-        # res +=     self.alpha  * (self.MSSSIM(lab_x[:, :1, :, :],
-        #                                       lab_tar[:, :1, :, :]))
+        res +=     self.alpha  * (self.MSSSIM(lab_x[:, :1, :, :],
+                                              lab_tar[:, :1, :, :]))
 
-        # return res
+        return res / 100
